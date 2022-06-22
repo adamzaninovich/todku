d45bc4d875cd70452ea5cb537b403ef583559694
Adam Zaninovich <adam.zaninovich@gmail.com>
2022-06-22 09:35:19 -0700

    add handle_params to about live

--- lib/todku_web/live/about_live/about_live.ex
+++ lib/todku_web/live/about_live/about_live.ex
@@ -5,4 +5,9 @@ defmodule TodkuWeb.AboutLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
+
+  @impl true
+  def handle_params(_params, _url, socket) do
+    {:ok, socket}
+  end
 end
